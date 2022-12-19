89305cb12e00295c0b827e9a84fe2c8f508834b3
UnknownPersik <UnknownPersik@gmail.com>
2022-12-19 19:31:07 +0500

    refactoring

--- src/main/java/parser/JsonParser.java
+++ src/main/java/parser/JsonParser.java
@@ -5,10 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
-public class JSONParser {
+public class JsonParser {
     private static final ObjectMapper objectMapper = new ObjectMapper();
-
-    public String[] parser(byte[] info) throws IOException {
+    public WeatherEntity parser(byte[] info) throws IOException {
         JsonNode jsonNode = objectMapper.readTree(info);
         JsonNode weatherData = jsonNode.path("weather");
         String weather = weatherData.elements().next().path("main").asText();
@@ -17,6 +16,6 @@ public class JSONParser {
         String feelTemp = jsonNode.path("main").path("feels_like").asText();
         String humidity = jsonNode.path("main").path("humidity").asText();
         String city = jsonNode.path("name").asText();
-        return new String[]{weather, wind, temp, feelTemp, humidity, city};
+        return new WeatherEntity(weather, wind, temp, feelTemp, humidity, city);
     }
 }
